f4865d6bc6aa5aa9e90669c45b63c7f5922a0c9d
OscarGB <oscar.gomez.borz@gmail.com>
2017-03-29 13:03:42 +0200

    TestPregunta

---
+++ Practica 3/Noodle/src/pruebas/EstadisticasAlumnoTest.java
@@ -0,0 +1,19 @@
+package pruebas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EstadisticasAlumnoTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
